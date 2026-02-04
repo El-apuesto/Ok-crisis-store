b3bed7fc43dd286e4638d7fa4f1ef2b870f10e37
El-apuesto <thecitieschoice@gmail.com>
2026-02-04 06:27:31 -0600

    Switch from HashRouter to BrowserRouter for proper Cloudflare routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './contexts/CartContext';
 import { LandingPage } from './pages/LandingPage';
 import { CartPage } from './pages/CartPage';
